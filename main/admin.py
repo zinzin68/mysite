2640f43cfeeba265270005f10da11cb883663638
zinzin68 <dnwls3958@naver.com>
2023-05-16 10:36:11 +0900

    post page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+#게시글(Post) Model을 불러옵니다.
+from .models import Post
 
 # Register your models here.
+#관리자가 게시글에 접근 가능
+admin.site.register(Post)
--- main/models.py
+++ main/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Post(models.Model):
     postname = models.CharField(max_length=50)
     contents = models.TextField()
+
+    def __str__(self):
+        return self.postname
