c2c28a7fee912bd80c434766492a0951b2b10adf
zinzin68 <dnwls3958@naver.com>
2023-05-16 11:32:34 +0900

    post detail

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
+from django.contrib import admin
 from django.urls import path
 
-from . import views
+from main.views import index, blog, posting
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('blog/',views.blog, name='blog'),
+    path('admin/', admin.site.urls),
+    # 웹사이트의 첫화면은 index 페이지이다 + URL이름은 index이다
+    path('', index, name='index'),
+    # URL:80/blog에 접속하면 blog 페이지 + URL이름은 blog이다
+    path('blog/', blog, name='blog'),
+    # URL:80/blog/숫자로 접속하면 게시글-세부페이지(posting)
+    path('blog/<int:pk>',posting, name='posting'),
 ]
